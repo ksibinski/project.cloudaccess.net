e195062a355c0dd26c3926a6333c0056bba5b1f7
Kris Sibinski <k.sibinski@gmail.com>
2016-12-07 18:08:53 +0100

    add chapter1 directory and today.php file

---
+++ chapter1/today.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Today&rsquo;s Date</title>
+</head>
+Installation 21
+<body>
+<p>Today&rsquo;s date (according to this web server) is
+<?php
+echo date('l, F jS Y.');
+?>
+</p>
+</body>
+</html>
