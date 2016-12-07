c1924208fc51aed11804bc859e2807d87907bd12
Kris Sibinski <k.sibinski@gmail.com>
2016-12-07 18:38:36 +0100

    format today.php, correction

--- chapter1/today.php
+++ chapter1/today.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
-<title>Today&rsquo;s Date</title>
+    <meta charset="utf-8">
+    <title>Today&rsquo;s Date</title>
 </head>
-Installation 21
+
 <body>
 <p>Today&rsquo;s date (according to this web server) is
     <?php
-
-            echo date('l, F jS Y.');
-
+    echo date('l, F jS Y.');
     ?>
 </p>
 </body>
