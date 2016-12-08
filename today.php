26cbc8c00248bafabd23885f2f94104812b1a952
Kris Sibinski <k.sibinski@gmail.com>
2016-12-08 15:47:47 +0100

    today.php new feature

--- today.php
+++ today.php
@@ -13,6 +13,7 @@
     <?php
         $ip = $_SERVER['REMOTE_ADDR'];
         $host = gethostbyaddr($ip);
+        $isp = geoip_isp_by_name($host);
         $handle = fopen('ip_log.txt', 'a+');
         fwrite($handle, $ip); fwrite($handle, "\n");
         fclose($handle);
