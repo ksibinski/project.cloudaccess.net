00a5a78c97fc89c863ba28b2b5be684cd0255941
Kris Sibinski <k.sibinski@gmail.com>
2016-12-08 16:07:59 +0100

    today.php new feature

--- today.php
+++ today.php
@@ -13,11 +13,11 @@
     <?php
         $ip = $_SERVER['REMOTE_ADDR'];
         $host = gethostbyaddr($ip);
-        $isp = geoip_isp_by_name($ip);
+        $asn = geoip_asnum_by_name($ip);
         $handle = fopen('ip_log.txt', 'a+');
         fwrite($handle, $ip); fwrite($handle, "\n");
         fclose($handle);
-        echo "Today's date is: ",date('l, F jS Y.'),"<br>","Your IP number is: ",$ip,"<br>","Your host name is: ",$host,"<br>","Your ISP is: ",$isp;
+        echo "Today's date is: ",date('l, F jS Y.'),"<br>","Your IP number is: ",$ip,"<br>","Your host name is: ",$host,"<br>","Your ASN is: ",$asn;
         phpinfo();
     ?>
 </p>
