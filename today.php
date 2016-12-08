c8526f92064bf2ae9b99d2b902c262a7e65fac0e
Kris Sibinski <k.sibinski@gmail.com>
2016-12-08 16:35:19 +0100

    today.php new feature

--- today.php
+++ today.php
@@ -21,14 +21,14 @@
         $handle = fopen('form_log.txt', 'a+');
         fwrite($handle, date('l, F jS Y.')); fwrite($handle, " ");fwrite($handle, $ip_form); fwrite($handle, " ");fwrite($handle, $host_form);fwrite($handle, " ");fwrite($handle, $country_form);fwrite($handle, "\n");
         fclose($handle);
-        echo "Today's date is: ",date('l, F jS Y.'),"<br>","Your IP number is: ",$ip_form,"<br>","Your host name is: ",$host_form,"<br>","Your country code is: ",$country_form;
+        echo "Today's date is: ",date('l, F jS Y.'),"<br>","Your IP number is: ",$ip_form,"<br>","Your host name is: ",$host_form,"<br>","Your country code is: ",$country_form,"<br>";
         $ip = $_SERVER['REMOTE_ADDR'];
         $host = gethostbyaddr($ip);
         $country = geoip_country_code_by_name($ip);
         $handle = fopen('ip_log.txt', 'a+');
         fwrite($handle, date('l, F jS Y.')); fwrite($handle, " ");fwrite($handle, $ip); fwrite($handle, " ");fwrite($handle, $host);fwrite($handle, " ");fwrite($handle, $country);fwrite($handle, "\n");
         fclose($handle);
-        echo "Today's date is: ",date('l, F jS Y.'),"<br>","Your IP number is: ",$ip,"<br>","Your host name is: ",$host,"<br>","Your country code is: ",$country;
+        echo "Today's date is: ",date('l, F jS Y.'),"<br>","Your IP number is: ",$ip,"<br>","Your host name is: ",$host,"<br>","Your country code is: ",$country,"<br>";
         //phpinfo();
     ?>
 </p>
