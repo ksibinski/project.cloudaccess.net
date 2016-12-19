89c5e44833fb356791eb55a173f442649f2ddecb
Kris Sibinski <k.sibinski@gmail.com>
2016-12-19 17:16:03 +0100

    today.php new feature

--- ipform.php
+++ ipform.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="utf-8">
     <title>IP Check Form</title>
-    <style>
-        h1 {text-align:center;}
-        p {text-align:center;}
-    </style>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
 </head>
 <body>
 <form action="ipform.php" method="post">
